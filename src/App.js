6c8b3d8c93a914792724ceb06aa87f1017dc9983
roopantkalra <roopantkalra@gmail.com>
2023-01-04 19:10:41 +0530

    adding textform component and building feature for converting text to uppercase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform'
 
 
 function App() {
   return (
 <>
 <Navbar title="Text Utility" />
+<div className="container my-5">
+<Textform heading="Enter your text below to analyze"/>
+</div>
 </>
   )
 }
